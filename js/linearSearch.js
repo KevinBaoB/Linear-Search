0255fa0b57b69c8a9c0e2e920335861c25a09a12
KevinBaoB <kevinbaobui@gmail.com>
2022-05-25 14:55:00 -0500

    First

--- js/linearSearch.js
+++ js/linearSearch.js
@@ -1,9 +1,27 @@
-const arrayToSearchThrough = [];
-for (let i = 1; i <= 1000; i++) {
-    arrayToSearchThrough.push(i);
+function linear(word) {
+  let arrayToSearchThrough = [];
+  for (let i = 0; i < word.length; i++) {
+    arrayToSearchThrough.push(word[i]);
+  }
+  return arrayToSearchThrough;
 }
 
-exports.linearSearch = function(valueToFind, arrayToSearchThrough) {
+bananaPhone = linear("banana");
 
+// console.log(bananaPhone);
+
+// exports.linearSearch = function(valueToFind, arrayToSearchThrough) {
+
+// };
+
+const linearSearch = (item, arrayToSearchThrough) => {
+  let arr = [];
+  for (i = 0; i < arrayToSearchThrough.length; i++) {
+    if (arrayToSearchThrough[i] === item) {
+      arr.push(i);
+    }
+  }
+  return arr;
 };
 
+console.log(linearSearch("a", bananaPhone));
